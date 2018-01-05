ab4df8d477e6b8f9cd4c8bef992c66e8a9e4a45a
Mucyo Miller <mucyomiller@gmail.com>
2018-01-05 21:28:10 +0000

    disable profile temporary

--- app/routes.js
+++ app/routes.js
@@ -47,11 +47,11 @@ module.exports = function(app, passport) {
 	// =====================================
 	// we will want this protected so you have to be logged in to visit
 	// we will use route middleware to verify this (the isLoggedIn function)
-	app.get('/profile', isLoggedIn, function(req, res) {
-		res.render('profile2.ejs', {
-			user : req.user // get the user out of session and pass to template
-		});
-	});
+	// app.get('/profile', isLoggedIn, function(req, res) {
+	// 	res.render('profile2.ejs', {
+	// 		user : req.user // get the user out of session and pass to template
+	// 	});
+	// });
 
 	// =====================================
 	// LOGOUT ==============================
